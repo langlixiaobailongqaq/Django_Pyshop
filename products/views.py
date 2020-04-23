434cee4a2f5fb443c336e12cf643e5e589473798
unknown <1544717589@qq.com>
2020-04-23 10:24:30 +0800

    B站上6小时Python基础，Django基础知识

---
+++ products/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+
+def index(request):
+	products = Product.objects.all()
+	return render(request, 'index.html',
+	              {'products': products })
+
+
+def new(request):
+	return HttpResponse('111')
